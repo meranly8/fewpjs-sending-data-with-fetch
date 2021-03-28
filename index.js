8abca0bf5572e6e62ca822f7a58a979ea74c87cc
Michelle Ranly <meranly8@gmail.com>
2021-03-28 02:20:56 -0500

    commit tree 809ab8eb581f112a922a7c1b1ba2c0637d965bfe

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    }
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+    return fetch("http://localhost:3000/users", configObj)
+    .then(resp => resp.json())
+    .then(object => document.body.innerHTML = object["id"])
+    .catch(error => document.body.innerHTML = error.message)
+}
